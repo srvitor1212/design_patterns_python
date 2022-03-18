19bcf7f52680028cfc43fc6d90cc740a6ecb7473
Vitor Vicente <vitor.v@ambevtech.com.br>
2022-03-18 14:31:06 -0300

    Command e Template Method

---
+++ Comportamentais/Observer1.py
@@ -0,0 +1,46 @@
+
+
+class Objeto:
+
+    def __init__(self):
+        self.__observadores = []
+    
+    def __repr__(self):
+        return '::Objeto::'
+    
+    def registrar(self, observador):
+        self.__observadores.append(observador)
+    
+    def notificar_todos(self, *args, **kwargs):
+        for observador in self.__observadores:
+            observador.notificar(self, *args, **kwargs) # notificar é a função dos observadores A e B
+
+
+class ObservadorA:
+
+    def __init__(self, objeto):
+        objeto.registrar(self) # é a função do Objeto
+    
+    def notificar(self, objeto, *args):
+        print(f'O {type(self).__name__} recebeu uma {args[0]} de {objeto}')
+
+
+class ObservadorB:
+
+    def __init__(self, objeto):
+        objeto.registrar(self) # é a função do Objeto
+    
+    def notificar(self, objeto, *args):
+        print(f'O {type(self).__name__} recebeu uma {args[0]} de {objeto}')
+
+
+
+
+print('\n\n\n')
+print('----------------------------------------------------------------------------------------')
+obj = Objeto()
+
+obs_a = ObservadorA(obj)
+obs_b = ObservadorB(obj)
+
+obj.notificar_todos('notificação')
